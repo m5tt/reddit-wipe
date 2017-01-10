09a529418046a246be33b1de6778dbf710a48a16
m5tt <arks36@protonmail.ch>
2017-01-09 21:40:15 -0500

    wrote setup

--- setup.py
+++ setup.py
@@ -1,2 +1,17 @@
-import unittest
-import redditwipe.redditwipe as r
+from setuptools import setup, find_packages
+
+setup(
+    name='redditwipe',
+    version='0.1.dev',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'reddit-wipe = redditwipe.redditwipe:main'
+        ]
+    },
+    install_requires=['praw'],
+    license='MIT',
+    url='https://github.com/m5tt/reddit-wipe',
+    author='m5tt',
+    author_email='arks36@protonmail.com'
+)
