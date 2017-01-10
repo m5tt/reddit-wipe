d669c5c4767913e7cf8441f6fc19d01d0fff53bd
m5tt <arks36@protonmail.ch>
2017-01-09 23:31:34 -0500

    Changed up to deal with reddits limitations

--- redditwipe/redditwipe.py
+++ redditwipe/redditwipe.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import re
+import time
 import socket
 import signal
 import argparse
@@ -17,6 +18,9 @@ USER_AGENT = 'reddit-wipe'
 CONTENT_TYPE_COMMENT = 'comments'
 CONTENT_TYPE_SUBMISSION = 'submissions'
 
+COOLDOWN_COUNT = 1000       # reddit limitation
+COOLDOWN_TIME = 10
+
 
 def ctrl_c_exit(signum, frame):
     """
@@ -28,22 +32,27 @@ def ctrl_c_exit(signum, frame):
     exit(0)
 
 
-def delete_content(content, item_matcher, replace_str, no_delete):
-    for item in content:
-        if item_matcher(item):
-            item.edit(replace_str)
-            if not no_delete:
-                item.delete()
+def delete_content(user, exclude, item_matcher, replace_str, no_delete):
+    content = get_content(user, exclude)
+
+    while len(list(content)):
+        for item in content:
+            if item_matcher(item):
+                item.edit(replace_str)
+                if not no_delete:
+                    item.delete()
+        time.sleep(COOLDOWN_TIME)
+        content = get_content(user, exclude)
 
 
 def get_content(user, exclude):
     if exclude == CONTENT_TYPE_COMMENT:
-        content = user.submissions.new(limit=None)
+        content = user.submissions.new()
     elif exclude == CONTENT_TYPE_SUBMISSION:
-        content = user.comments.new(limit=None)
+        content = user.comments.new()
     else:
-        content = itertools.chain(user.comments.new(limit=None),
-                                  user.submissions.new(limit=None))
+        content = itertools.chain(user.comments.new(),
+                                  user.submissions.new())
 
     return content
 
@@ -78,14 +87,12 @@ def is_connected():
 
 
 def get_args():
-    arg_parser = argparse.ArgumentParser(description='A commandline reddit content deleter')
+    arg_parser = argparse.ArgumentParser(description='A command line reddit content deleter')
 
     arg_parser.add_argument('--exclude', choices=[CONTENT_TYPE_COMMENT, CONTENT_TYPE_SUBMISSION],
                             default='', required=False, type=str, help='exclude either comments or submissions')
     arg_parser.add_argument('--replace-str', default='', required=False, type=str,
                             help='What to replace the comment body with before deleting')
-    arg_parser.add_argument('--no-delete', action='store_true', required=False,
-                            help='only replace comment/submission bodies with a string')
     match_group = arg_parser.add_mutually_exclusive_group()
     match_group.add_argument('--keyword', default='', required=False, type=str,
                              help='Delete only comments containing a keyword')
@@ -106,10 +113,8 @@ def main():
         except:                             # praw's exceptions are all wacked out so just do this
             print('Error: failed to login')
         else:
-            content = get_content(user, args.exclude)
-
             item_matcher = get_item_matcher(args.keyword, args.pattern)
-            delete_content(content, item_matcher, args.replace_str, args.no_delete)
+            delete_content(user, args.exclude, item_matcher, args.replace_str, args.no_delete)
 
             print('All done')
     else:
